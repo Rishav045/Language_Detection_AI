b5e59ac5f643ec7100ecd3ea109cdc69e7adad89
Rishav045 <82647816+Rishav045@users.noreply.github.com>
2023-03-10 22:03:33 +0530

    adding functionity of translating of many languages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 import { options } from "./utils/fetch";
+import { languagesList } from "../languages";
+import { languages } from "../languages";
 
 const welcomes = [
   {lan: 'Malayalam', text: 'സ്വാഗതം'},
@@ -28,21 +30,15 @@ function App() {
   const [welcomesIndex, setWelcomesIndex] = useState(welcomes.length-1);
   const [translated, setTranslated] = useState("");
   const [txt, setTxt] = useState("English");
+  const [val, setVal] = useState("");
 
   const encodedParams = new URLSearchParams();
 encodedParams.append("q", content);
-encodedParams.append("target", "hi");
-encodedParams.append("source", "en");
+encodedParams.append("target", languages[val]);
+encodedParams.append("source", languages[txt]);
 console.log(import.meta.env.VITE_RAPID_API_KEY);
 options['body']=encodedParams
-console.log(options);
-const translate = async()=>{
-  fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-	.then(response => response.json())
-	.then(response => setTranslated(response.data.translations[0].translatedText))
-  
-	.catch(err => console.error(err));
-}
+
 
   const getData = async () => {
     setLoading(true)
@@ -63,6 +59,23 @@ const translate = async()=>{
       });
   };
 
+  const translate = async()=>{
+    getData();
+    console.log(options)
+    if(!val || !txt)
+    {
+       setTranslated("Error:- Please select the language")
+    }
+    else{
+      fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
+    .then(response => response.json())
+    .then(response => setTranslated(response.data.translations[0].translatedText))
+    
+    .catch(err => console.error(err));
+    }
+    
+  }
+
   return (
     <div style={{  height: '100vh', overflowX: 'hidden' }} >
       <nav style={{ width: '100%', height: '50px', position: 'fixed', top: 0, left: 0, display: 'flex', justifyContent: 'center', alignItems: 'center', backgroundColor: 'wheat' }} >
@@ -77,8 +90,17 @@ const translate = async()=>{
         <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }} >
           <h2 style={{ color: '#000' }} >I can detect <a href="#languages" style={{ color: 'blue', textDecoration: 'underline', cursor: 'pointer' }} >17 languages</a></h2>
           <input className="input" type="text" placeholder="Hi" value={content} onChange={(event) => {setContent(event.target.value)}} />
-          <button disabled={loading} onClick={getData} style={{ color: loading ? '#000' : '#FFF', backgroundColor: loading ? '#5F5F5F' : '#000' }} >Click me</button>
+          <button disabled={loading} onClick={getData} style={{ color: loading ? '#000' : '#FFF', backgroundColor: loading ? '#5F5F5F' : '#000' }} >Detect Language</button>
           <h1 style={{ color: '#000', textAlign: 'center' }} >{txt}</h1>
+          
+          <input list="data" onChange={(e)=>setVal(e.target.value)} placeholder="Search Language"
+          style={{padding:'10px',margin:'8px' ,borderRadius:'2px' ,width:'200px'}}/>
+          <datalist id="data" >
+            {
+              languagesList.map((lan)=><option>{lan}</option>)
+            }
+          </datalist>
+          {console.log(val)}
           <button disabled={loading} onClick={translate} style={{ color: loading ? '#000' : '#FFF', backgroundColor: loading ? '#5F5F5F' : '#000' }} >Translate</button>
           <h1 style={{ color: '#000', textAlign: 'center' }} >{translated}</h1>
         </div>
